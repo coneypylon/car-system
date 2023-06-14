dd954722153f7be627ff289b2c7199497b1384db
Nick Boyko <coneypylon@gmail.com>
2023-06-14 00:40:24 +0000

    working server

--- modelrailwaybiller/modelrailwaybiller/wsgi.py
+++ modelrailwaybiller/modelrailwaybiller/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modelwaybiller.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modelrailwaybiller.settings')
 
 application = get_wsgi_application()
