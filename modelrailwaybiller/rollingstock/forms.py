8828e7e1375db45e560c1d30a6ddb140ee600cf9
Nick Boyko <coneypylon@gmail.com>
2023-06-25 01:25:26 -0400

    n/f car creation modal

--- modelrailwaybiller/rollingstock/forms.py
+++ modelrailwaybiller/rollingstock/forms.py
@@ -2,4 +2,9 @@ from django import forms
 
 
 class StockForm(forms.Form):
-    road_name = forms.CharField(label="Road Name:", max_length=100)
+    road_name = forms.CharField(label="Reporting Mark:", max_length=4)
+    id_number = forms.IntegerField(label="Registration Number")
+    aar_type = forms.CharField(label="AAR Type:", max_length=2)
+    cargo = forms.CharField(label="Cargo carried:", max_length=100)
+    loaded = forms.BooleanField(label="Loaded?")
+    location = forms.CharField(label="Current Location:")
--- modelrailwaybiller/rollingstock/views.py
+++ modelrailwaybiller/rollingstock/views.py
@@ -8,26 +8,30 @@ from .models import RailVehicle
 
 @login_required
 def index(request):
+    # receive da form
+    if request.method == "POST":
+        form = StockForm(request.POST)
+        if form.isvalid():
+            # guess I'm gonna lern how to use form.cleaned_data
+            return HttpResponse("Stock Received!")
+    else:
+        form = StockForm()
+    
     road_listed_vehicles = RailVehicle.objects.order_by('reporting_mark','id_number')
     #output = ', '.join(str(q) for q in road_listed_vehicles)
     #template = loader.get_template('rollingstock/index.html')
     context = {
         'road_listed_vehicles': road_listed_vehicles,
+        'form': form,
     }
     #return HttpResponse(template.render(context,request))
     return render(request, 'rollingstock/index.html', context)
 
 @login_required
 def get_new_stock(request):
-    # receive da form
-    if request.method == "POST":
-        form = StockForm(request.POST)
-    if form.isvalid():
-        # guess I'm gonna lern how to use form.cleaned_data
-        return HttpResponse("Stock Received!")
     
-    else:
-        form = StockForm()
+    
+    
     
     return render(request, "newstock.html", {"form": form})
 
